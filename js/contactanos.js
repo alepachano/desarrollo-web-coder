8bc1261b66b72ca8c894d66f0d01c44ffa0aac8f
avpachano <av.pachano@gmail.com>
2021-05-11 18:55:06 -0400

    se incorpora animacion de jquery, e icono de carrito al menu de todas las paginas

--- js/contactanos.js
+++ js/contactanos.js
@@ -1,11 +1,28 @@
 // Evento jQuery al seleccionar el input
 
+var inputForm = $(".form-control");
+var iconCarrito = $("#iconoCarrito");
+
 $(document).ready(function(){
-    $(".form-control").focus(function(){
+    inputForm.focus(function(){
         $(this).css("background-color", "#dbfdff");
     });
 
-    $(".form-control").blur(function(){
+    inputForm.blur(function(){
         $(this).css("background-color", "white");
     });
 });
+
+iconCarrito.mouseenter(function(){
+    $(this).animate({
+        height: "50px",
+        width: "50px"
+    }, 'slow')
+})
+
+iconCarrito.mouseleave(function(){
+    $(this).animate({
+        height: "30px",
+        width: "30px"
+    }, 'fast')
+})
