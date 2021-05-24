45def45d08e9da0976a5c99ebf5e1b6b39c8074c
avpachano <av.pachano@gmail.com>
2021-05-24 19:36:27 -0400

    se coloca footer en HTML externo, y se enlaza en cada pagina HTML mediante js

--- js/tortas-y-postres.js
+++ js/tortas-y-postres.js
@@ -96,17 +96,13 @@ function renderizarSeccion(seccion, categoria) {
     seccion.innerHTML = mostrarProductos(categoria);
 }
 
-// LLAMAR FUNCIONES 
-renderizarSeccion(seccionTortasClasicas, categoriaTortasClasicas);
-validarLocalStorage();
-
 // IDENTIFICA ID DEL PRODUCTO Y AGREGA EL ID CARRITO
 function identificarId(identificadorProducto) {
     const productoBuscado = productos.find((producto) => producto.id === identificadorProducto);
     agregarAlCarrito(productoBuscado);
     const alerta = document.getElementById('alertaAgregarAlCarrito');
     alerta.innerHTML = 
-        `<div class="mt-3 alert alert-success" role="alert">
+        `<div class="mt-2 alert alert-success" role="alert">
             ¡Se ha agregado un producto al carrito de compras!
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
@@ -133,6 +129,7 @@ function agregarAlCarrito(producto) {
     }
 }
 
+// EVENTO: click sobre la categoria
 function eventoClick(tab, seccion, nombreCategoria){
     tab.addEventListener("click", function(){
         seccion.innerHTML = mostrarProductos(nombreCategoria);
@@ -144,3 +141,6 @@ eventoClick(tabTortasClasicas, seccionTortasClasicas, categoriaTortasClasicas);
 eventoClick(tabTortasPremium, seccionTortasPremium, categoriaTortasPremium);
 eventoClick(tabPostresCandybar, seccionPostresCandybar, categoriaPostres);
 
+// LLAMAR FUNCIONES 
+renderizarSeccion(seccionTortasClasicas, categoriaTortasClasicas);
+validarLocalStorage();
